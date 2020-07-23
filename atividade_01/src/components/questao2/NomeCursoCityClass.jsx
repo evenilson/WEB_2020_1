ac4ca67e9a0561be92d22a416c34835706a44878
evenilson <evenilsonlp@gmail.com>
2020-07-23 16:30:55 -0300

    Questao2 feita

---
+++ atividade_01/src/components/questao2/NomeCursoCityClass.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class NomeCursoCityClass extends Component{
+    render(){
+        return (
+            <div>
+                <h1>Nome: Evenilson Liandro</h1>
+                <h1>Curso: Sisitemas de Informação</h1>
+                <h1>Cidade de origem: Canindé</h1>
+            </div>
+        )
+    }
+}
--- atividade_01/src/index.js
+++ atividade_01/src/index.js
@@ -2,6 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import NomeCursoCity from './components/questao1/NomeCursoCity'
+import NomeCursoCityClass from './components/questao2/NomeCursoCityClass'
 
+//Questao1
+/*
 const root = document.getElementById('root');
 ReactDOM.render(<NomeCursoCity/>,root);
+*/
+
+//Questão2
+const root = document.getElementById('root');
+ReactDOM.render(<NomeCursoCityClass/>,root);
