5a6d4a48a33e03b089cb5576fe03210535ab18ed
evenilson <evenilsonlp@gmail.com>
2020-07-23 14:46:52 -0300

    Questão1 feita

---
+++ atividade_01/src/components/questao1/NomeCursoCity.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NomeCursoCity(){
+    return(
+        <div>
+            <h1>Nome: Evenilson Liandro</h1>
+            <h1>Curso: Sisitemas de Informação</h1>
+            <h1>Cidade de origem: Canindé</h1>
+        </div>
+    )
+}
+
+export default NomeCursoCity
